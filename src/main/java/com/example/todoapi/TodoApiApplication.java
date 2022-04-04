97ae527983bc60b1377f46f7087b0958796582b7
poco-tech <86156985+poco-tech@users.noreply.github.com>
2022-04-04 22:36:47 +0900

    Spring Initializr で Spring Boot プロジェクトの雛形を作成しよう

---
+++ src/main/java/com/example/todoapi/TodoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApiApplication.class, args);
+	}
+
+}
