85adb520429a935a63f6fb94d4edae431b1b534c
poco-tech <86156985+poco-tech@users.noreply.github.com>
2022-04-07 22:46:18 +0900

    ヘルスチェックAPIを実装してみよう

--- src/main/java/com/example/todoapi/controller/HealthController.java
+++ src/main/java/com/example/todoapi/controller/HealthController.java
@@ -1,4 +1,13 @@
 package com.example.todoapi.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthController implements HealthApi {
+
+    @Override
+    public ResponseEntity<Void> healthGet() {
+        return ResponseEntity.ok().build();
+    }
 }
