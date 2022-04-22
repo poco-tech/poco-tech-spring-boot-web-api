9db7d8c1cd0b7ed454605619b043f939e787088e
poco-tech <86156985+poco-tech@users.noreply.github.com>
2022-04-22 22:04:37 +0900

    Dependency Injection を使ってみよう

--- src/main/java/com/example/todoapi/controller/sample/SampleController.java
+++ src/main/java/com/example/todoapi/controller/sample/SampleController.java
@@ -1,6 +1,7 @@
 package com.example.todoapi.controller.sample;
 
 import com.example.todoapi.service.sample.SampleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,10 @@ import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/samples")
+@RequiredArgsConstructor
 public class SampleController {
 
-    private final SampleService service = new SampleService();
+    private final SampleService service;
 
     @GetMapping
     public SampleDTO index() {
--- src/main/java/com/example/todoapi/service/sample/SampleService.java
+++ src/main/java/com/example/todoapi/service/sample/SampleService.java
@@ -1,5 +1,8 @@
 package com.example.todoapi.service.sample;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SampleService {
 
     public SampleEntity find() {
