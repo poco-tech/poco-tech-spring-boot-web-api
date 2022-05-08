95eb712110ca272d607b5750b3b7fd930fb36f1d
poco-tech <86156985+poco-tech@users.noreply.github.com>
2022-05-08 22:28:53 +0900

    /tasks/1 (empty response body)：Controller を実装しよう

---
+++ src/main/java/com/example/todoapi/controller/task/TaskController.java
@@ -0,0 +1,14 @@
+package com.example.todoapi.controller.task;
+
+import com.example.todoapi.controller.TasksApi;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController implements TasksApi {
+
+    @Override
+    public ResponseEntity<Void> tasks1Get() {
+        return ResponseEntity.ok().build();
+    }
+}
