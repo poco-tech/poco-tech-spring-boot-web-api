fb9523322843f53760252998a4393e53233ef646
poco-tech <86156985+poco-tech@users.noreply.github.com>
2022-05-15 22:31:00 +0900

    /tasks/1：Controller を実装しよう

--- src/main/java/com/example/todoapi/controller/task/TaskController.java
+++ src/main/java/com/example/todoapi/controller/task/TaskController.java
@@ -1,6 +1,7 @@
 package com.example.todoapi.controller.task;
 
 import com.example.todoapi.controller.TasksApi;
+import com.example.todoapi.model.TaskDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class TaskController implements TasksApi {
 
     @Override
-    public ResponseEntity<Void> showTask() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<TaskDTO> showTask() {
+        var body = new TaskDTO();
+        body.setId(1L);
+        body.setTitle("TEST");
+        return ResponseEntity.ok(body);
     }
 }
