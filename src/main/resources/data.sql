3a2eb2abdf9fdd49450f56815b38aeb9ca4f77ca
poco-tech <86156985+poco-tech@users.noreply.github.com>
2022-05-16 12:55:00 +0900

    data.sql でタスクテーブルにデータを投入しよう

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,7 @@
 INSERT INTO samples (content) VALUES ('Hello, world!!');
+
+INSERT INTO tasks (title) VALUES ('タスク1');
+INSERT INTO tasks (title) VALUES ('タスク2');
+INSERT INTO tasks (title) VALUES ('タスク3');
+INSERT INTO tasks (title) VALUES ('タスク4');
+INSERT INTO tasks (title) VALUES ('タスク5');
