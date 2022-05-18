3fe523de150178335a8a502cb8db70ea901cb513
poco-tech <86156985+poco-tech@users.noreply.github.com>
2022-05-18 11:39:18 +0900

    404 レスポンス：ボディを返すようにハンドラーを変更しよう

--- src/main/java/com/example/todoapi/controller/advice/CustomExceptionHandler.java
+++ src/main/java/com/example/todoapi/controller/advice/CustomExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.todoapi.controller.advice;
 
+import com.example.todoapi.model.ResourceNotFoundError;
 import com.example.todoapi.service.task.TaskEntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,7 +11,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class CustomExceptionHandler {
 
     @ExceptionHandler(TaskEntityNotFoundException.class)
-    public ResponseEntity<Void> handleTaskEntityNotFoundException(TaskEntityNotFoundException e) {
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<ResourceNotFoundError> handleTaskEntityNotFoundException(TaskEntityNotFoundException e) {
+        var error = new ResourceNotFoundError();
+        error.setDetail(e.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(error);
     }
 }
