564f6d2d8509266d67f917cf45c4d1d87295f809
poco-tech <86156985+poco-tech@users.noreply.github.com>
2022-05-19 22:26:20 +0900

    400 Bad Request（フィールド欠損）：例外ハンドラー実装

--- src/main/java/com/example/todoapi/controller/advice/CustomExceptionHandler.java
+++ src/main/java/com/example/todoapi/controller/advice/CustomExceptionHandler.java
@@ -1,14 +1,19 @@
 package com.example.todoapi.controller.advice;
 
+import com.example.todoapi.model.BadRequestError;
 import com.example.todoapi.model.ResourceNotFoundError;
 import com.example.todoapi.service.task.TaskEntityNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class CustomExceptionHandler {
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(TaskEntityNotFoundException.class)
     public ResponseEntity<ResourceNotFoundError> handleTaskEntityNotFoundException(TaskEntityNotFoundException e) {
@@ -18,5 +23,15 @@ public class CustomExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(error);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request
+    ) {
+        return ResponseEntity.badRequest().body(new BadRequestError());
     }
 }
