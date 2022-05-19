e44e91a96692e2deea0e365ddcc3d174c5ca0763
poco-tech <86156985+poco-tech@users.noreply.github.com>
2022-05-19 10:37:35 +0900

    POST されたら 201 を返す：Controller

--- src/main/java/com/example/todoapi/controller/task/TaskController.java
+++ src/main/java/com/example/todoapi/controller/task/TaskController.java
@@ -4,6 +4,7 @@ import com.example.todoapi.controller.TasksApi;
 import com.example.todoapi.model.TaskDTO;
 import com.example.todoapi.service.task.TaskService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,10 @@ public class TaskController implements TasksApi {
         dto.setTitle(entity.getTitle());
 
         return ResponseEntity.ok(dto);
+    }
+
+    @Override
+    public ResponseEntity<Void> createTask() {
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
