745a2c9b4ee44a9d20087f64c8bd890e471ab1ce
poco-tech <86156985+poco-tech@users.noreply.github.com>
2022-05-24 14:10:55 +0900

    200 レスポンス（Taskリソース）：Controller

--- src/main/java/com/example/todoapi/controller/task/TaskController.java
+++ src/main/java/com/example/todoapi/controller/task/TaskController.java
@@ -3,12 +3,14 @@ package com.example.todoapi.controller.task;
 import com.example.todoapi.controller.TasksApi;
 import com.example.todoapi.model.TaskDTO;
 import com.example.todoapi.model.TaskForm;
+import com.example.todoapi.model.TaskListDTO;
 import com.example.todoapi.service.task.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -41,7 +43,10 @@ public class TaskController implements TasksApi {
     }
 
     @Override
-    public ResponseEntity<Void> listTasks() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<TaskListDTO> listTasks() {
+        var dto = new TaskListDTO();
+        dto.setResults(List.of(new TaskDTO(), new TaskDTO()));
+
+        return ResponseEntity.ok(dto);
     }
 }
